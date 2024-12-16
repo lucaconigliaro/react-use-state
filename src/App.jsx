58e2f5206c85371ce18b82036dc7328ea2d67377
lucaconigliaro <lucaconigliaro1@gmail.com>
2024-12-16 13:36:16 +0100

    feat: Buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,20 @@ function App() {
  
   return (
     <>
-      
+      <header className="mb-5">
+        <h1>Learn Web developement</h1>
+      </header>
+      <main>
+        <div className="container">
+          <div className="d-flex flex-wrap gap-2 mb-4">
+          {languages.map((curLanguage) => (
+          <button key={curLanguage.id} className="btn btn-primary">
+            {curLanguage.title}
+          </button>
+        ))}
+          </div>
+        </div>
+      </main>
     </>
   )
 }
