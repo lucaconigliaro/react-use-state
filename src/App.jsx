2bd2c739f8e701149983a3eabbe2e8361bfd7af4
lucaconigliaro <lucaconigliaro1@gmail.com>
2024-12-16 14:15:08 +0100

    feat: First Language

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react"
 import languages from "./data/languages"
 
 function App() {
- 
+  const [activeLanguage, setActiveLanguage] = useState(languages[0])
+
   return (
     <>
       <header className="mb-5">
@@ -10,14 +12,18 @@ function App() {
       <main>
         <div className="container">
           <div className="d-flex flex-wrap gap-2 mb-4">
-          {languages.map((curLanguage) => (
-          <button key={curLanguage.id} className="btn btn-primary">
-            {curLanguage.title}
-          </button>
-        ))}
+            {languages.map((curLanguage) => (
+              <button key={curLanguage.id} className="btn btn-primary">
+                {curLanguage.title}
+              </button>
+            ))}
+          </div>
+          <div className="card p-3">
+            <h2>{activeLanguage.title}</h2>
+            <p>{activeLanguage.description}</p>
           </div>
         </div>
-      </main>
+    </main >
     </>
   )
 }
